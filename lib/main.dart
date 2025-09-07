7023924fbd27733cf7c66f1f81a636d73d1bb155
Threshika <threshikavijay@gmail.com>
2025-09-07 17:20:11 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/weather_screen.dart';
+
+void main() {
+  runApp(const WeatherApp());
+}
+
+class WeatherApp extends StatelessWidget{
+  const WeatherApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //making the debug banner removed
+      debugShowCheckedModeBanner: false,
+      //setting the theme for entire app globally instead of setting bgcolor for every page and use material3
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const WeatherScreen(),
+    );
+  }
+}
+
